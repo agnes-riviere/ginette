1acecfa05d968fbe8e59b70562b32c9e3329e98e
agnes-riviere <agnes.riviere1@gmail.com>
2020-05-27 15:45:08 +0200

    modifier la legende

--- application/mini-LOMOS/PLOT/temperature_time_series.R
+++ application/mini-LOMOS/PLOT/temperature_time_series.R
@@ -2,11 +2,12 @@ library(ggplot2)
 library(stringr)
 library(RColorBrewer)
 #paths
-path_output <- "../GINETTE_SENSI/OUTPUT/"
-path_plot <- "./"
-path_obs <- "../GINETTE_SENSI/OBS/"
-
-setwd(path_plot)
+path_output <- "GINETTE_SENSI/OUTPUT/"
+path_plot <- "../"
+#path_obs <- "../GINETTE_SENSI/OBS/"
+path_obs <- "~/Programmes/ginette/application/mini-LOMOS/GINETTE_SENSI/OBS/"
+#setwd(path_plot)
+setwd("~/Programmes/ginette/application/mini-LOMOS")
 sim_name = 1
 date_bg = "03/05/2019 00:00:00"
 date_bg = as.POSIXct(date_bg, '%d/%m/%Y %H:%M', tz = 'GMT')
@@ -20,7 +21,7 @@ pos_bg = str_locate(files_output, "Sim_temperature_maille")
 nPT100 = length(files_obs)
 # Depth PT100
 Inversion_PT100 = "inversion_PT100.COMM"
-depth_PT100 = read.csv(paste0("../", Inversion_PT100),
+depth_PT100 = read.csv(paste0(Inversion_PT100),
                        sep = " ",
                        header = FALSE) #écart entre les PT100 en cm
 depth_PT100[1, 1] = -depth_PT100[1, 1]
@@ -36,26 +37,31 @@ depth_PT100 = depth_PT100 / 100
 #obs dataframe
 df_obs_t = read.table(paste0(path_obs, files_obs[1]),
                       header = FALSE)
+
+colnames(df_obs_t) =  c("time", paste0(depth_PT100$V1[1], " m"))
 if (nPT100 >= 2) {
-  for (i in seq(2, nPT100)) {
-    a <-
-      read.table(paste0(path_obs, "Obs_temperature_maille", i, "_t.dat"))
-    df_obs_t <- cbind(df_obs_t, a$V2)
-  }
+for (i in seq(2, nPT100)) {
+  a <-
+    read.table(paste0(path_obs, "Obs_temperature_maille", i, "_t.dat"))
+ df_obs_t <- cbind(df_obs_t, a[,2])
+  
 }
-if (nPT100 < 2)
-  colnames(df_obs_t) =  c("time", paste0(depth_PT100$V1[1], " m"))
-if (nPT100 == 2)
+}
+
+
+if (nPT100 == 2) {
   colnames(df_obs_t) =  c("time",
                           paste0(depth_PT100$V1[1], " m"),
                           paste0(depth_PT100$V1[2], " m"))
-if (nPT100 >= 3)
+}
+if (nPT100 >= 3) {
   colnames(df_obs_t) = c(
     "time",
     paste0(depth_PT100$V1[1], " m"),
     paste0(depth_PT100$V1[2], " m"),
     paste0(depth_PT100$V1[3], " m")
   )
+}
 Melted_obs_t <- reshape2::melt(df_obs_t, id.var = "time")
 colnames(Melted_obs_t) = c("time", "Depth", "Temperature")
 Melted_obs_t$time = Melted_obs_t$time + date_bg
@@ -112,10 +118,11 @@ g_temp_ts <-
     mapping = aes(x = time, y = Temperature, color = Depth),
     linetype = "dashed"
   ) +
-  scale_color_manual(values = c(colpal, colpal)) +
+  scale_color_manual(values = c(colpal, colpal,colpal, colpal)) +
   labs(x = "", y = "T (C)", color = "depth") +
   scale_x_datetime(date_labels = " %d %b") +
-  theme_bw() 
+  theme_bw() + 
+  scale_linetype_manual(values = c(2, 2, 2, 2)) 
 
 png(paste0("PLOT/Data_check/", "lol", ".png"))
   g_temp_ts
