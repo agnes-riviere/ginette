56405a4939c0fadae24ae164cd10ae0c9624644c
agnes-riviere <agnes.riviere@mines-paristech.fr>
2021-12-02 17:40:33 +0100

    modification for Ramon

--- application/ZNS-1D/compile_ginette.py
+++ application/ZNS-1D/compile_ginette.py
@@ -32,9 +32,14 @@ dt=900
 #duration of the simulation in days
 nb_day=100
 
+# state
+## 0 steady state
+# 1 transient state (dynamic state)
+state=0
+
 #in meter
-z_top=0.55
-dz=0.01
+z_top=40
+dz=0.033
 
 #apply unsaturated flow and thermal 
 #unsat =1 apply
@@ -44,22 +49,26 @@ unsat=1
 
 nb_zone=1
 # user-defined  parameters zone 1 
-# intrinsic permeability
-val_k=1e-11
+# intrinsic permeability [m2]  k=K*mu/(rho*g)
+# K hydraulic conductivity [m.s-1]
+# mu viscosity [Pa.s]
+# rho density [kg.m-3]
+# g gravity  9.81 [m2.s-1]
+val_k=3.33333333333333e-15
 # porosity
-val_n=0.3
-# solid density r=val_r 
-val_r=1600
+val_n=0.38 # \Phi
+# solid grain density rho_s=val_r  [kg.m-3]
+val_r=2578
 # Van Genuchten parameters
-val_a=6.64 #m-1
-val_nVG= 2.03
-val_swres=0.01
+val_a=2.00000 #m-1 alpha_vg
+val_nVG= 1.23  # n_vg
+val_swres=0.26 # S_wr
 
 
 
 #Boundary conditions hydraulic head h=P/rho g + Z
-top=0 
-bot=-8
+top=15 
+bot=15
 
 
 ## write the parameters
@@ -70,6 +79,7 @@ nb_cell=z_top/dz
 
 
 setup_model=setup_model.replace('[dt]','%06.0fD+00' % dt)
+setup_model=setup_model.replace('[state]','%1i' % state)
 setup_model=setup_model.replace('[nb_day]','%06.0f' % nb_day)
 setup_model=setup_model.replace('[z_top]', '%7.3e' % z_top)
 setup_model=setup_model.replace('[az]','%7.3e' % z_top)
@@ -100,7 +110,7 @@ coord = pd.read_csv(f_coor, names=["id", "x", "z"], header=None, delim_whitespac
 param_zone=param_zone.replace('[k1]','%8.2e' % val_k)
 param_zone=param_zone.replace('[n1]','%6.2f' % val_n)
 param_zone=param_zone.replace('[r1]','%6.2f' % val_r)
-param_zone=param_zone.replace('[a1]','%6.2f' % val_a)
+param_zone=param_zone.replace('[a1]','%8.2e' % val_a)
 param_zone=param_zone.replace('[nVG1]','%6.2f' % val_nVG)
 param_zone=param_zone.replace('[swres1]','%6.2f' % val_swres)
 
@@ -132,3 +142,15 @@ f_coor.close()
 
 subprocess.call(["./ginette"])   
 
+#saturation_profile = pd.read_csv('S_saturation_profil_t.dat',sep=' ', index_col=0)
+
+
+#plt.figure()
+#plt.style.use('seaborn')
+#Tair = df['Orly']
+#Teau = df['Temp']
+#plt.scatter(Tair, Teau, s=10, alpha=1, color='mediumblue',marker='.')
+#plt.xlabel('Tair (°c)')
+#plt.ylabel('Teau (°c)')
+#df.plot(x='Orly', y='Temp', figsize=(19,4))
+#plt.title('Liens Tair et Teau pour la Seine à Paris')
