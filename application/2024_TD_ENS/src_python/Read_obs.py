6fcb2ff7dcacd23ce7667dfe70c779840283f21e
agnes-riviere <agnes.riviere@minesparis.psl.eu>
2024-10-23 10:33:12 +0200

    convert all format of dates

--- application/2024_TD_ENS/src_python/Read_obs.py
+++ application/2024_TD_ENS/src_python/Read_obs.py
@@ -1,5 +1,81 @@
 import os
 import pandas as pd
+import numpy as np
+
+def rename_columns(df):
+    """
+    Rename columns based on specific conditions.
+    
+    Parameters:
+    - df: pandas DataFrame with columns to be renamed.
+    
+    Returns:
+    - df: pandas DataFrame with renamed columns.
+    """
+    def column_mapper(col_name):
+        if 'temperature1' in col_name.lower():
+            return 'Temp1'
+        elif 'temperature2' in col_name.lower():
+            return 'Temp2'
+        elif 'stream' in col_name.lower() or 'molo' in col_name.lower():
+            return 'TempMolo'
+        else:
+            return col_name
+
+    df = df.rename(columns=column_mapper)
+    return df
+
+
+def convert_dates(df: pd.DataFrame, date_column: str):
+    """
+    Convert dates from a list of strings by testing several different input formats.
+    Try all date formats already encountered in data points.
+    If none of them is OK, try the generic way (None).
+    If the generic way doesn't work, this method fails
+    (in that case, you should add the new format to the list).
+    
+    This function works directly on the given Pandas dataframe (in place).
+    This function assumes that the specified column of the given Pandas dataframe
+    contains the dates as character string type.
+    
+    For datetime conversion performance, see:
+    See https://stackoverflow.com/questions/40881876/python-pandas-convert-datetime-to-timestamp-effectively-through-dt-accessor
+    """
+    formats = [
+        "%m/%d/%y %H:%M:%S", "%m/%d/%y %I:%M:%S %p",
+        "%d/%m/%y %H:%M",    "%d/%m/%y %I:%M %p",
+        "%m/%d/%Y %H:%M:%S", "%m/%d/%Y %I:%M:%S %p", 
+        "%d/%m/%Y %H:%M",    "%d/%m/%Y %I:%M %p",
+        "%y/%m/%d %H:%M:%S", "%y/%m/%d %I:%M:%S %p", 
+        "%y/%m/%d %H:%M",    "%y/%m/%d %I:%M %p",
+        "%Y/%m/%d %H:%M:%S", "%Y/%m/%d %I:%M:%S %p", 
+        "%Y/%m/%d %H:%M",    "%Y/%m/%d %I:%M %p",
+        "%d/%m/%Y %H:%M",    "%d/%m/%Y %H:%M:%S", "%Y-%m-%d %H:%M:%S",
+        None
+    ]
+    
+    times = df[date_column]
+    for f in formats:
+        try:
+            # Convert strings to datetime objects
+            new_times = pd.to_datetime(times, format=f)
+            # Convert datetime series to numpy array of integers (timestamps)
+            new_ts = new_times.values.astype(np.int64)
+            # If times are not ordered, this is not the appropriate format
+            test = np.sort(new_ts) - new_ts
+            if np.sum(abs(test)) != 0:
+                raise ValueError()
+            # Else, the conversion is a success
+            df[date_column] = new_times
+            return
+        
+        except (ValueError, TypeError):
+            continue
+    
+    # None of the known formats are valid
+    raise ValueError("Cannot convert dates: No known formats match your data!")
+    return df
+
 
 def process_obs_data(Obs_data, date_simul_bg, coef, ost, nb_day):
     # Obtenez la liste des fichiers dans le répertoire
@@ -16,17 +92,16 @@ def process_obs_data(Obs_data, date_simul_bg, coef, ost, nb_day):
                 all_data = pd.read_csv(os.path.join(Obs_data, fichier), sep=';')
             elif 'Temp' in fichier:
                 temp_data = pd.read_csv(os.path.join(Obs_data, fichier), sep=';')    
-
-    all_data['dates'] = pd.to_datetime(all_data['dates'], format='%d/%m/%Y %H:%M')
+    # Convert dates in the 'dates' column
+    convert_dates(all_data, 'dates')
     all_data = all_data[all_data['dates'] > date_simul_bg]
-
-    temp_data['dates'] = pd.to_datetime(temp_data['dates'], format='%d/%m/%Y %H:%M:%S')
+    convert_dates(temp_data,'dates')
     temp_data = temp_data[temp_data['dates'] > date_simul_bg]
 
     all_data = all_data.merge(temp_data, on=['dates'])
     # drop 2 columns # and ST2
     all_data = all_data.drop(columns=['ST2', '#'], inplace=False)
-    # rename columns
+
     all_data.columns = ['dates', 'deltaP', 'TempMolo', 'Temp1', 'Temp2', 'Temp3', 'Temp4']
 
     # Réindexer le DataFrame
@@ -74,6 +149,7 @@ def process_obs_data(Obs_data, date_simul_bg, coef, ost, nb_day):
         print(indices_not_equal_900_sec)
         # Afficher les lignes correspondantes dans le dataframe
         print(all_data.loc[indices_not_equal_900_sec])
+ 
     
     return all_data
 
